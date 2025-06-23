a31cabddd5fdfb11fa7b8b5bbebd24780d352bdd
saisoftsuave <sai.babu@softsuave.org>
2025-06-23 13:20:17 +0530

    refactor API endpoints to align with Python API structure

--- composeApp/src/commonMain/kotlin/org/softsuave/bustlespot/data/network/APIEndpoints.kt
+++ composeApp/src/commonMain/kotlin/org/softsuave/bustlespot/data/network/APIEndpoints.kt
@@ -2,17 +2,27 @@ package org.softsuave.bustlespot.data.network
 
 
 object APIEndpoints {
-    const val SIGNIN = "/api/auth/signin"
+/*
+    const val SIGNIN = "/api/auth/signin" // Node API
     const val SIGNOUT = "/api/auth/signout"
-
     const val GETALLORGANISATIONS ="/api/organisation/getUserOrganization"
-    const val GetOrganisation ="/organisation/get_organisation"
+
 
     const val GETALLPROJECTS = "/api/project/getProjectList"
+
+ */
     const val GETALLTASKS = "/api/task/getTaskByProjectId"
 
     const val POSTACTIVITY = "/api/activity/addActivityList"
     const val GETALLACTIVITIES = "/api/activity/get-all-activity"
 
     const val UPDATEACTIVITY = "/api/activity/updateActivity"
+
+
+    // python api
+    const val SIGNIN = "/auth/signin"
+    const val GETALLORGANISATIONS ="/organisation/get_all_organisation"
+    const val SIGNOUT = "/auth/signout"
+    const val GETALLPROJECTS = "/project/getProjectList"
+
 }
