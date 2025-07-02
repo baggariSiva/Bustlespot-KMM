7ff72056bd6274ea1b6e583c336815c488ca7ec7
SaiSrujan-SoftSuave <sai.srujan@softsuave.org>
2025-07-02 19:17:55 +0530

    refactor: update HomeViewModel to handle modules and improve project/task selection
    This commit refactors the `HomeViewModel` to introduce the concept of modules.
    
    Key changes:
    - Added module selection dropdown and related state management (`_selectedModule`, `_moduleDropDownState`).
    - Modified `getAllProjects` to fetch projects based on the selected module ID.
    - Updated `fetchAllTasksForProjects` to take a list of `Project` objects.
    - `constructPostActivityRequest` now uses nullable `taskId` and `projectId` and includes latitude/longitude if "Onsite" module is selected.
    - Adjusted `postUserActivity` and related functions to use the updated `ActivityData` structure.
    - Enhanced `DropDownEvents` and `DropDownState` to support modules.
    - Added `ShowModuleChangeDialog` to `TrackerDialogEvents`.
    - Improved error handling and UI feedback for dropdown selections.
    - `updateSelectedTaskTime` now correctly handles Float values for time and untracked time.
    - `checkTaskAndProject` now also checks if a module is selected.

---
+++ composeApp/src/desktopMain/kotlin/org/softsuave/bustlespot/main.kt
@@ -0,0 +1,124 @@
+package org.softsuave.bustlespot
+
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowDecoration
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import bustlespot.composeapp.generated.resources.Res
+import bustlespot.composeapp.generated.resources.logoRed
+import com.github.kwhat.jnativehook.GlobalScreen
+import com.mmk.kmpnotifier.notification.NotifierManager
+import com.mmk.kmpnotifier.notification.configuration.NotificationPlatformConfiguration
+import org.jetbrains.compose.resources.painterResource
+import org.softsuave.bustlespot.di.initKoin
+import java.awt.Button
+import java.awt.Desktop
+import java.awt.Dialog
+import java.awt.FlowLayout
+import java.awt.Frame
+import java.awt.Label
+import java.net.ServerSocket
+import javax.swing.JOptionPane
+import javax.swing.SwingUtilities
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    // Prevent multiple instances using a lock on a specific port
+    val singleInstancePort = 65432
+    val lockSocket: ServerSocket = try {
+        ServerSocket(singleInstancePort).also {
+            Runtime.getRuntime().addShutdownHook(Thread {
+                try {
+                    it.close()
+                } catch (_: Exception) {
+                }
+            })
+        }
+    } catch (e: Exception) {
+        JOptionPane.showMessageDialog(
+            null,
+            "Bustlespot is already running.",
+            "Instance Already Running",
+            JOptionPane.WARNING_MESSAGE
+        )
+        return
+    }
+    Thread.setDefaultUncaughtExceptionHandler { _, e ->
+        print("asdasd")
+        e.printStackTrace()
+        print("erorr ${e} is here")
+        Dialog(Frame(), e.message ?: "Error").apply {
+            layout = FlowLayout()
+            val label = Label(e.message)
+            add(label)
+            val button = Button("OK").apply {
+                addActionListener { dispose() }
+            }
+            add(button)
+            setSize(300,300)
+            isVisible = true
+        }
+    }
+
+    application {
+        // Cleanup on close
+        Runtime.getRuntime().addShutdownHook(Thread {
+            try {
+                GlobalScreen.unregisterNativeHook()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        })
+
+        NotifierManager.initialize(
+            NotificationPlatformConfiguration.Desktop(
+                showPushNotification = true
+            )
+        )
+
+        initKoin()
+
+        Window(
+            title = "Bustlespot",
+            decoration = WindowDecoration.SystemDefault,
+            state = rememberWindowState(size = DpSize(width = 460.dp, height = 785.dp)),
+            onCloseRequest = ::exitApplication,
+            resizable = false,
+            icon = painterResource(Res.drawable.logoRed),
+        ) {
+            App(
+                onFocusReceived = {
+                    val desktop = Desktop.getDesktop()
+                    if (desktop.isSupported(Desktop.Action.APP_EVENT_FOREGROUND)) {
+                        // works for Mac OS
+                        desktop.requestForeground(true)
+                        window.toFront()
+                        window.requestFocus()
+                    } else {
+                        //works for Windows
+                        if (window.state == Frame.ICONIFIED) {
+                            window.state = Frame.NORMAL
+                        }
+                        SwingUtilities.invokeLater {
+                            window.isAlwaysOnTop = true
+                            window.toFront()
+                            window.requestFocus()
+                            window.isAlwaysOnTop = false
+                        }
+                    }
+                },
+                onMinimizeClick = {
+                    window.let {
+                        it.isMinimized = false
+                        SwingUtilities.invokeLater { it.isMinimized = true }
+                    }
+                },
+                onCloseClick = {
+                    exitApplication()
+                }
+            )
+        }
+    }
+}
