8a0fe93fa687d755f748370f9633f948815c961c
SaiSrujan-SoftSuave <sai.srujan@softsuave.org>
2025-07-02 15:41:54 +0530

    Add `moveToFirst` extension function to List
    This commit introduces a new extension function `moveToFirst` for the `List` class. This function allows moving a specified item to the beginning of the list if it exists in the list.

--- composeApp/src/commonMain/kotlin/org/softsuave/bustlespot/data/network/Constants.kt
+++ composeApp/src/commonMain/kotlin/org/softsuave/bustlespot/data/network/Constants.kt
@@ -1,5 +1,5 @@
 package org.softsuave.bustlespot.data.network
 
 //const val BASEURL = "https://bustlespot-api.gamzinn.com" //ngrok URL Maybe differs
-const val BASEURL = "https://clever-wood-95394.pktriot.net" // PKTriot URL
-
+//const val BASEURL = "https://clever-wood-95394.pktriot.net" // PKTriot PROD URL
+const val BASEURL = "https://58e7-14-98-153-162.ngrok-free.app" // NRGOK Dev URL
