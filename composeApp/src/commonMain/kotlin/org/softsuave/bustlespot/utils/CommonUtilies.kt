d11b3bd64c3cfa3969f00bfc10ee31a0ad6a8a1f
SaiSrujan-SoftSuave <sai.srujan@softsuave.org>
2025-07-02 15:41:43 +0530

    Add `moveToFirst` extension function to List
    This commit introduces a new extension function `moveToFirst` for the `List` class. This function allows moving a specified item to the beginning of the list if it exists in the list.

--- composeApp/src/commonMain/kotlin/org/softsuave/bustlespot/utils/CommonUtilies.kt
+++ composeApp/src/commonMain/kotlin/org/softsuave/bustlespot/utils/CommonUtilies.kt
@@ -14,3 +14,20 @@ expect fun handleBackPress(
 
 val BustleSpotRed : Color
     get() = Color(242, 60, 75, 255)
+
+
+fun <T> List<T>.moveToFirst(item: T): List<T> {
+    val mutableList = this.toMutableList()
+    if (mutableList.remove(item)) { // Only move if the item exists
+        mutableList.add(0, item)
+    }
+    return mutableList
+}
+//
+//fun getCurrentDateTime(): String {
+//    val zoneId = ZoneId.of("Asia/Kolkata") // +05:30
+//    val currentDateTime = ZonedDateTime.now(zoneId)
+//
+//    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSSXXX")
+//    return currentDateTime.format(formatter)
+//}
