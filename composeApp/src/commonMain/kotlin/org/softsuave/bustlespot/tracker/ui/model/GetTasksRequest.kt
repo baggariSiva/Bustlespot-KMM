402e5d30be5c41548899103039f63012b7951aff
SaiSrujan-SoftSuave <sai.srujan@softsuave.org>
2025-07-02 15:42:28 +0530

    Remove unused data classes GetProjectRequest and GetTasksRequest
    These data classes were no longer needed and have been removed to clean up the codebase.

--- composeApp/src/commonMain/kotlin/org/softsuave/bustlespot/tracker/ui/model/GetTasksRequest.kt
+++
@@ -1,9 +0,0 @@
-package org.softsuave.bustlespot.tracker.ui.model
-
-import kotlinx.serialization.Serializable
-
-
-@Serializable
-data class GetTasksRequest(
-    val projectId: String, val organisationId: String
-)
