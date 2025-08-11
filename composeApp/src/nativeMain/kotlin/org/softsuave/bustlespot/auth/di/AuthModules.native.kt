fedaa905ee0de064050d397b0abf544091033ab6
saisoftsuave <sai.babu@softsuave.org>
2025-08-11 12:43:44 +0530

    Add PostingServiceManager to platformModule for dependency injection

--- composeApp/src/nativeMain/kotlin/org/softsuave/bustlespot/auth/di/AuthModules.native.kt
+++ composeApp/src/nativeMain/kotlin/org/softsuave/bustlespot/auth/di/AuthModules.native.kt
@@ -2,7 +2,8 @@ package org.softsuave.bustlespot.auth.di
 
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import org.softsuave.bustlespot.background.PostingServiceManager
 
 actual val platformModule: Module = module {
-
+    single { PostingServiceManager() }
 }
