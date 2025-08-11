f06c5180c53490926a4f5182dbb89810468205f8
saisoftsuave <sai.babu@softsuave.org>
2025-08-11 12:44:16 +0530

    Enhance PostingServiceManager with new posting functionality and state management

--- composeApp/src/nativeMain/kotlin/org/softsuave/bustlespot/background/PostingServiceManager.kt
+++ composeApp/src/nativeMain/kotlin/org/softsuave/bustlespot/background/PostingServiceManager.kt
@@ -1,11 +1,11 @@
 package org.softsuave.bustlespot.background
 
+import kotlinx.coroutines.flow.StateFlow
+import org.softsuave.bustlespot.utils.ActivityServiceState
+
 
 actual class PostingServiceManager() {
 
-    actual fun startPosting(postData: String) {
-
-    }
 
     actual fun stopPosting() {
     }
@@ -22,6 +22,22 @@ actual class PostingServiceManager() {
 
     actual fun getActiveTaskCount(): Int {
         return 0
+    }
+
+    actual val currentState: StateFlow<ActivityServiceState>?
+        get() = null
+
+    actual fun startPosting(
+        postData: String,
+        initialTimeMillis: Long,
+        taskId: String?,
+        projectId: String?,
+        onStart: () -> Unit
+    ) {
+    }
+
+    actual fun getCurrentElapsedTime(): Long {
+        return 0L
     }
 
 }
